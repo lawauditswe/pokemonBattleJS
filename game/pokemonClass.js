3c4e856f571f7b586cd66bd033fe85225508cb5d
TX Ho <tingxianho@gmail.com>
2022-03-29 17:11:23 +0100

    Pokemon class created

---
+++ game/pokemonClass.js
@@ -0,0 +1,67 @@
+class Pokemon {
+	constructor(
+		name,
+		nature,
+		ability,
+		type1,
+		move1,
+		baseHP,
+		baseAtk,
+		baseDef,
+		baseSpA,
+		baseSpD,
+		baseSpe,
+		hpEVs,
+		atkEVs,
+		defEVs,
+		spaEVs,
+		spdEVs,
+		speEVs,
+		hpIVs,
+		atkIVs,
+		defIVs,
+		spaIVs,
+		spdIVs,
+		speIVs,
+		gender,
+		level,
+		type2,
+		move2,
+		move3,
+		move4,
+		statusCondition
+	) {
+		this.name = name;
+		this.nature = nature;
+		this.ability = ability;
+		this.type1 = type1;
+		this.move1 = move1;
+		this.baseHP = baseHP;
+		this.baseAtk = baseAtk;
+		this.baseDef = baseDef;
+		this.baseSpA = baseSpA;
+		this.baseSpD = baseSpD;
+		this.baseSpe = baseSpe;
+		this.hpEVs = hpEVs;
+		this.atkEVs = atkEVs;
+		this.defEVs = defEVs;
+		this.spaEVs = spaEVs;
+		this.spdEVs = spdEVs;
+		this.speEVs = speEVs;
+		this.hpIVs = hpIVs;
+		this.atkIVs = atkIVs;
+		this.defIVs = defIVs;
+		this.spaIVs = spaIVs;
+		this.spdIVs = spdIVs;
+		this.speIVs = speIVs;
+		this.gender = gender;
+		this.level = level;
+		this.type2 = type2;
+		this.move2 = move2;
+		this.move3 = move3;
+		this.move4 = move4;
+		this.statusCondition = statusCondition;
+	}
+}
+
+let rillaboom = new Pokemon();
