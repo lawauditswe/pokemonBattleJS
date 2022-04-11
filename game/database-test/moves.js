503477d0a68fd5be2fee9fd486063701d873fab0
TX Ho <tingxianho@gmail.com>
2022-04-11 21:43:21 +0100

    initialized npm

---
+++ game/database-test/moves.js
@@ -0,0 +1,12 @@
+// import moveClass from '../../classes/moveClass';
+// const moveClass = require('')
+
+// const thunderbolt = new Move('Thunderbolt', 'electric', 'special', 90, 100, 24);
+
+// const surf = new Move('Surf', 'water', 'special', 90, 100, 24);
+
+// const quickAttack = new Move('Quick Attack', 'normal', 'physical', 40, 100, 48);
+
+// const ironTail = new Move('Iron Tail', 'steel', 'physical', 100, 75, 24);
+
+// console.log(thunderbolt);
