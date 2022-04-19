ad9030aecb1092485dac501da4cd4f859397dd3a
TX Ho <tingxianho@gmail.com>
2022-04-19 22:18:27 +0100

    Got exports working, Pokemon name rendering in app component

--- pokemon-battle/src/gameMechanics/criticalHit.js
+++ pokemon-battle/src/gameMechanics/criticalHit.js
@@ -1,5 +1,5 @@
 const criticalHit = () => {
-	crit = Math.floor(Math.random() * 100);
+	const crit = Math.floor(Math.random() * 100);
 
 	if (crit % 24 === 0) return 6144 / 4096;
 	return 1;
---
+++ pokemon-battle/src/testPokemon/testPokemon.js
@@ -0,0 +1,31 @@
+import Pokemon from '../gameClasses/pokemonClass';
+import { thunderbolt } from '../testMoves/testMoves';
+
+export const pikachu = new Pokemon(
+	'Pikachu',
+	'modest',
+	'lightning rod',
+	'electric',
+	thunderbolt,
+	35,
+	55,
+	40,
+	50,
+	50,
+	90,
+	0,
+	252,
+	4,
+	252,
+	0,
+	0,
+	30,
+	31,
+	31,
+	31,
+	31,
+	31,
+	'Male',
+	100,
+	null
+);
